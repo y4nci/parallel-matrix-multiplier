bbbe95a4eb1399b95283414237e277533ccfe8a1
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2023-05-07 02:15:47 +0300

    check for ready rows & columns each iteration

--- src/Semaphore.cpp
+++ src/Semaphore.cpp
@@ -29,6 +29,10 @@ void* addRows1_2(void* args) {
         cellInfoMatrix1_2->at(rowNumber)[i] = READY;
     }
 
+    /**
+     * we only have to check for ready columns in 3_4, because we already know that we
+     * just completed a row in 1_2. so, we don't need to check for ready rows in 1_2.
+     */
     signalMultiplier = checkForReadyColumns(cellInfoMatrix3_4);
 
     if (signalMultiplier) {
@@ -75,16 +79,23 @@ void* addRows3_4(void* args) {
     for (int i = 0; i < row1.size(); i++) {
         outputMatrix->getMatrixValues()[rowNumber][i] = row1[i] + row2[i];
         cellInfoMatrix3_4->at(rowNumber)[i] = READY;
-    }
 
-    signalMultiplier = checkForReadyRows(cellInfoMatrix1_2);
+        /**
+         * unlike addRows1_2, we have to check for both ready columns in 3_4
+         * and ready rows in 1_2, and we have to do it at every iteration.
+         * the reason is, we can create new columns ready for multiplication
+         * at every iteration, so we have to check for them.
+         */
+        signalMultiplier = checkForReadyRows(cellInfoMatrix1_2)
+                && checkForReadyColumns(cellInfoMatrix3_4);
 
-    if (signalMultiplier) {
-        // send signal to multiplier thread, it will take care of the rest
-        sem_post(rowColumnSemaphore);
+        if (signalMultiplier) {
+            // send signal to multiplier thread, it will take care of the rest
+            sem_post(rowColumnSemaphore);
 
-        // wait for multiplier thread to finish
-        sem_wait(multiplierSemaphore);
+            // wait for multiplier thread to finish
+            sem_wait(multiplierSemaphore);
+        }
     }
 
     ////////////////////////////
@@ -101,7 +112,18 @@ void* multiplyRowWithColumn(void* args) {
     CellInfoMatrix* writtenInfoMatrix1_2 = arguments->cellInfoMatrices.first;
     CellInfoMatrix* writtenInfoMatrix3_4 = arguments->cellInfoMatrices.second;
 
-    // TODO
+    // wait until a new row and column is ready
+    sem_wait(rowColumnSemaphore);
+
+    // TODO: detect which row and column is ready
+
+    // multiply the row with the column
+
+    // write the result to the output matrix
+
+    // mark the cell as written
+
+    // signal the adder thread
 
     pthread_exit(NULL);
 }
